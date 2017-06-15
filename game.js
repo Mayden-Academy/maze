71e8f6ffe1251a44d28fe3023251c68a0bf25155
Mike Oram Front End Developer <mike.oram@mayden.co.uk>
2017-06-15 13:57:00 +0100

    project

---
+++ game.js
@@ -0,0 +1,32 @@
+var totalSeconds = 0
+var timer
+
+function setTime() {
+	++totalSeconds;
+	$('#timer span').html(padLeft(Math.floor(totalSeconds/60), "00") + ':' + padLeft(totalSeconds%60, "00"))
+}
+
+function play() {
+	$('rect').not('.finish, .path').hover(function() {
+		resetPlay('You Died!')
+	})
+	
+	$('.finish').hover(function() {
+		resetPlay('Congratulations, you win! Time: ' + $('#timer span').html())
+	})
+	
+	timer = setInterval(setTime, 1000)
+}
+
+function padLeft(str, pad) {
+	str = str + ''
+	return pad.substring(0, pad.length - str.length) + str
+}
+
+function resetPlay(output) {
+	clearInterval(timer)
+	totalSeconds = 0
+	$('rect').off()
+	alert(output)
+}
+
